94686021fb8451e497b1abf66b4d7ee3ba30fcc0
armymp <67810944+armymp@users.noreply.github.com>
2025-12-08 10:26:19 -0500

    add get event by id with error handling server logging

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/armymp/event-booking-api/config"
 	"github.com/armymp/event-booking-api/db"
@@ -26,7 +30,9 @@ func main() {
 
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+
 	server.Run(fmt.Sprintf(":%d", port))
 }
 
@@ -39,6 +45,46 @@ func getEvents(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventIDStr := context.Param("id")
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		slog.Error("Failed to parse event ID from URL parameter",
+					"http_method", context.Request.Method,
+					"request_path", context.Request.URL.Path,
+					"event_id_string", eventIDStr,
+					"error_details", err.Error(), 
+		)
+
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id. Ensure it is a number."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			slog.Info("Event ID not found in database",
+						"event_id", eventID,
+						"request_path", context.Request.URL.Path)
+			
+			context.JSON(http.StatusNotFound, gin.H{"message": "Could not find event with provided event id"})
+			return
+		}
+
+		slog.Error("Database error while retrieving event ID",
+					"event_id", eventID,
+					"error_details", err.Error(),
+					"http_method", context.Request.Method)
+
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "An internal server error occurred."})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
