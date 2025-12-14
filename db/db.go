d8fbe1a735994cf9d80c5e9eb05ff5328aa2e0f9
armymp <67810944+armymp@users.noreply.github.com>
2025-12-14 09:09:39 -0500

    add users table and link to events table with foreign key and foreign key constraint

--- db/db.go
+++ db/db.go
@@ -24,6 +24,10 @@ func InitDB() {
 		log.Fatalf("Could not connect to database: %v", err)
 	}
 
+	if _, err := DB.Exec("PRAGMA foreign_keys = ON;"); err != nil {
+		log.Fatalf("Failed to enable foreign key enforcement: %v", err)
+	}
+
 	// Optional: connection pool settings
 	DB.SetMaxOpenConns(0)
 	DB.SetMaxIdleConns(2)
@@ -32,16 +36,30 @@ func InitDB() {
 }
 
 func createTables() {
-	createEventsTable := `CREATE TABLE IF NOT EXISTS events (
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	);
+	`
+	_, err := DB.Exec(createUsersTable)
+	if err != nil {
+		log.Fatalf("Could not create users table: %v", err)
+	}
+
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
-	);`
-
-	_, err := DB.Exec(createEventsTable)
+		user_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+	`
+	_, err = DB.Exec(createEventsTable)
 	if err != nil {
 		log.Fatalf("Could not create events table: %v", err)
 	}
